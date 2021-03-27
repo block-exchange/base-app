bc6957a51077a96881ec410d2530f658f4bf3e2d
aditya singh <adityasingh27@hotmail.com>
2021-03-27 21:41:35 +0530

    base app components

---
+++ web/index.jsx
@@ -0,0 +1,25 @@
+import React ,  { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+import {
+  HashRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+
+function App(props) {
+
+  return (
+        <div>
+		</div>
+  );
+}
+
+
+
+
+export default App
